c8debb9b5e3a091e6c951cb32a4e508ed9d42bf7
Yeregorix <yeregorixcraft@gmail.com>
2024-09-09 22:10:40 +0200

    Cleanup PluginMetadataUtils

--- forge/src/launch/java/org/spongepowered/forge/launch/plugin/ForgePluginContainer.java
+++ forge/src/launch/java/org/spongepowered/forge/launch/plugin/ForgePluginContainer.java
@@ -29,8 +29,6 @@ import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.spongepowered.common.launch.Launch;
-import org.spongepowered.forge.launch.ForgeLaunch;
 import org.spongepowered.plugin.PluginContainer;
 import org.spongepowered.plugin.metadata.PluginMetadata;
 
@@ -58,7 +56,7 @@ public class ForgePluginContainer implements PluginContainer {
     @Override
     public PluginMetadata metadata() {
         if (this.pluginMetadata == null) {
-            this.pluginMetadata = ((ForgeLaunch) Launch.instance()).metadataForMod((ModInfo) this.modContainer.getModInfo());
+            this.pluginMetadata = PluginMetadataConverter.modToPlugin((ModInfo) this.modContainer.getModInfo());
         }
         return this.pluginMetadata;
     }
